1634a8bdbb51e021ec1df38fd6e7624f201e3ae4
juanPabloCesarini <juanpcesarini@hotmail.com>
2021-03-23 20:51:16 -0300

    diseño del frame (color, borde, cursor, medida)

---
+++ Seccion 14/interfaz2.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()
+root.title("Matelab - mateGestion 1.0")
+root.resizable(0,0)
+root.iconbitmap("ISO_MATELAB.ico")
+
+miFrame = Frame(root)
+miFrame.pack(fill="x", expand=1)
+miFrame.config(width=800, height=600)
+miFrame.config(cursor="boat")
+miFrame.config(bg="green")
+miFrame.config(bd="5")
+miFrame.config(relief="ridge")
+
+
+
+
+
+root.mainloop()
